91b8e67ca0903376fe8fa287e52a027665ea56e7
444286 <147978579+444286@users.noreply.github.com>
2026-02-22 14:12:03 +0600

    Update Withdraw.js

--- models/Withdraw.js
+++ models/Withdraw.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const WithdrawSchema = new mongoose.Schema({
-  telegramId:String,
-  amount:Number,
-  status:{type:String, default:"pending"}
+  telegramId: String,
+  amount: Number,
+  method: String,
+  number: String,
+  status: { type: String, default: "pending" },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Withdraw", WithdrawSchema);
