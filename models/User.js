765d91d3f0af10b90e237e8656d35f48558853ab
444286 <147978579+444286@users.noreply.github.com>
2026-02-28 01:00:02 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,26 +1,57 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  telegramId: { type: String, unique: true },
-  username: String,
 
-  balance: { type: Number, default: 0 },
-  totalEarn: { type: Number, default: 0 },
+  telegramId: { 
+    type: String, 
+    unique: true, 
+    required: true 
+  },
 
-  todayAds: { type: Number, default: 0 },
+  username: { 
+    type: String, 
+    default: "No Name"   // 🔥 undefined fix
+  },
+
+  balance: { 
+    type: Number, 
+    default: 0 
+  },
+
+  totalEarn: { 
+    type: Number, 
+    default: 0 
+  },
+
+  todayAds: { 
+    type: Number, 
+    default: 0 
+  },
+
   lastAdDate: String,
   lastBonus: String,
 
   referralCode: String,
   referredBy: String,
-  inviteCount: { type: Number, default: 0 },
+
+  inviteCount: { 
+    type: Number, 
+    default: 0 
+  },
 
   deviceId: String,
   ipAddress: String,
 
-  blocked: { type: Boolean, default: false },
+  blocked: { 
+    type: Boolean, 
+    default: false 
+  },
 
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
+
 });
 
 module.exports = mongoose.model("User", UserSchema);
