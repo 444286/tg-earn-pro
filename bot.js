563dfc0a5576e04ca4ddda276b12b312eb730f32
444286 <147978579+444286@users.noreply.github.com>
2026-03-10 10:18:43 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const bot = new TelegramBot(process.env.BOT_TOKEN);
 
-bot.onText(/\/start/, (msg)=>{
+bot.onText(/\/start(.+)?/, (msg)=>{
 
 const chatId = msg.chat.id;
 const userId = msg.from.id;
