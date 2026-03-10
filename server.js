80ebc4f39e046ecfe650a182a205483a7d3c5499
444286 <147978579+444286@users.noreply.github.com>
2026-03-10 09:58:38 +0600

    Update server.js

--- server.js
+++ server.js
@@ -333,4 +333,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log("Server running"));
 
 
-require("./bot");
+const bot = require("./bot");
+
+app.post("/bot",(req,res)=>{
+bot.processUpdate(req.body);
+res.sendStatus(200);
+});
